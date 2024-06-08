087da8f7e9b3094a1323cd0f86dfe0b3c5016f07
Anthony Tamayo Ortega <tamayoortega.6@teleperformance.co>
2024-06-08 18:39:26 -0500

    i added double map to render the board

--- src/components/Board.js
+++ src/components/Board.js
@@ -25,24 +25,32 @@ export function Board({ xIsNext, squares, onPlay }) {
         status = 'Siguiente jugador: ' + (xIsNext ? 'X' : 'O');
     }
 
+    const renderSquare = (i) => (
+        <Square
+            key={i}
+            value={squares[i]}
+            onSquareClick={() => handleClick(i)}
+            squares={squares}
+        />
+    );
+
+    const boardSize = 3; // Número de filas y columnas en el tablero
+    const board = Array(boardSize).fill(null).map((_, rowIndex) => {
+        const rowSquares = Array(boardSize).fill(null).map((_, colIndex) => {
+            const squareIndex = rowIndex * boardSize + colIndex;
+            return renderSquare(squareIndex);
+        });
+        return (
+            <div key={rowIndex} className="board-row">
+                {rowSquares}
+            </div>
+        );
+    });
+
     return (
         <>
             <div className="status">{status}</div>
-            <div className="board-row">
-                <Square value={squares[0]} onSquareClick={() => handleClick(0)} squares={squares} />
-                <Square value={squares[1]} onSquareClick={() => handleClick(1)} squares={squares} />
-                <Square value={squares[2]} onSquareClick={() => handleClick(2)} squares={squares} />
-            </div>
-            <div className="board-row">
-                <Square value={squares[3]} onSquareClick={() => handleClick(3)} squares={squares} />
-                <Square value={squares[4]} onSquareClick={() => handleClick(4)} squares={squares} />
-                <Square value={squares[5]} onSquareClick={() => handleClick(5)} squares={squares} />
-            </div>
-            <div className="board-row">
-                <Square value={squares[6]} onSquareClick={() => handleClick(6)} squares={squares} />
-                <Square value={squares[7]} onSquareClick={() => handleClick(7)} squares={squares}/>
-                <Square value={squares[8]} onSquareClick={() => handleClick(8)} squares={squares} />
-            </div>
+            {board}
         </>
     );
 }
