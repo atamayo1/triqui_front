a61cfc96971755cb26ad55c00ca7c5c7969cee58
Anthony Tamayo Ortega <tamayoortega.6@teleperformance.co>
2024-06-08 19:03:21 -0500

    i delete an import that we dont need

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,5 +1,3 @@
-import { calculateWinner } from "../shared";
-
 export function Square({ value, onSquareClick, isWinningSquare  }) {
   return <button style={{ color: isWinningSquare ? 'red' : 'black' }} className="square" onClick={onSquareClick}>{value}</button>
 }
